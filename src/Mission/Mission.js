fd129a630cfc8cf2fcf580c7e4ed2e8907c4169a
Stepashka81 <ptashets81@gmail.com>
2023-02-11 23:54:41 +0200

    add state in component Mission

--- src/Mission/Mission.js
+++ src/Mission/Mission.js
@@ -1,23 +1,28 @@
 import '../App.css';
 import React from 'react';
 
-class Mission extends React.Component
-{
-  render(){
-  return (
-<>
-<h5>OUR MISSION</h5>
+class Mission extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            OurTagline: "We exist to help creators\nown their audience.",
+            OurHopes: "We believe that social platforms are taking advantage of creators.\nWe want to change that and help creators fight back."
+        }
 
-<h1> We exist to help creators<br></br>
-  own their audience.
-</h1>
-<h6>
-  We believe that social platforms are taking advantage of creators.<br></br>
-  We want to change that and help creators fight back.
-</h6>
-</>
+    }
 
+    render() {
+        const { OurTagline, OurHopes } = this.state;
+        
+        return (
+            <>
+                <h5>OUR MISSION</h5>
+                <h1> {OurTagline}  </h1>
+                <h6>{OurHopes}</h6>
+            </>
 
-  )}
+        )
+    }
 };
+
 export default Mission;
